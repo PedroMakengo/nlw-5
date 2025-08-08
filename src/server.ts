39fe32a3e954029d70bea048b01492589e4afbd0
PedroMakengo <pedromakengomiguel@gmail.com>
2025-08-08 10:49:02 +0100

    Feat: Controller separando as responsabilidades

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
-import express, { Request, Response } from "express";
-
+import express from "express";
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-/**
- * Tipos de metódos
- * GET: Buscar uma informação
- * POST: Criar uma informação
- * PUT: Atualizar uma informação
- * DELETE: Deletar uma informação
- * PATCH: Atualizar uma informação específica
- */
-
-app.get("/", (request: Request, response: Response) => {
-  return response.json({ message: "Hello World" });
-});
-
-app.post("/", (request: Request, response: Response) => {
-  return response.json({
-    message: "Usuário salvo com sucesso!",
-  });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
