33e8de9de6b3f29648d69b13ac701c88ab2bf87f
PedroMakengo <pedromakengomiguel@gmail.com>
2025-08-11 11:59:20 +0100

    Feat: Criando rota para pegar as mensagens dos usuários

--- src/services/MessagesService.ts
+++ src/services/MessagesService.ts
@@ -21,6 +21,18 @@ class MessagesService {
 
     return message;
   }
+
+  // List Messages by User
+  async listByUser(user_id: string) {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+
+    const list = await messagesRepository.find({
+      where: user_id,
+      relations: ["user"],
+    });
+
+    return list;
+  }
 }
 
 export { MessagesService };
